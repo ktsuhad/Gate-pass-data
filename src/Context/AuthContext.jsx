b21fe018c686a00ffdc40908082b9a2415eaca0d
SUHAD.KT <suhadkizhakethodi@gmail.com>
2023-11-02 16:13:54 +0530

    fix: Authentication issue resolved

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -3,14 +3,14 @@ import { createContext, useContext, useEffect, useState } from 'react';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(localStorage.getItem('token') || null);
+  const [token, setToken] = useState(null);
   const [isVerified, setIsVerified] = useState(false);
   const [profileData, setProfileData] = useState(JSON.parse(localStorage.getItem('profileData')) || null); // Retrieve profileData from localStorage
 
-  useEffect(() => {
-    // Whenever token or isVerified changes, update local storage
-    localStorage.setItem('token', token);
-  }, [token, isVerified]);
+  // useEffect(() => {
+  //   // Whenever token or isVerified changes, update local storage
+  //   localStorage.setItem('token', token);
+  // }, [token, isVerified]);
 
   useEffect(() => {
     localStorage.setItem('profileData', JSON.stringify(profileData));
